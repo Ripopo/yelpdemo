80444b6dfa2c40ba5f6b3593bb4d55359927168e
Katsuhiro Niitsuma <2110174k@gmail.com>
2015-11-29 22:32:07 +0900

    Link Reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129124359) do
+ActiveRecord::Schema.define(version: 20151129131058) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20151129124359) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
